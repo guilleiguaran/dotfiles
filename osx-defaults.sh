c5801b6fb09899e9c308b688c8e2d50910412caa
Guillermo Iguaran <guilleiguaran@gmail.com>
2019-04-20 20:13:52 -0500

    Add script to set defaults for OS X

---
+++ osx-defaults.sh
@@ -0,0 +1,98 @@
+# Based on https://gist.github.com/brandonb927/3195465
+
+################################################################################
+# General UI/UX
+################################################################################
+
+echo "Expanding the save panel by default"
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+echo "Automatically quit printer app once the print jobs complete"
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+echo "By default save to disk, rather than iCloud"
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+echo "Check for software updates daily, not just once per week"
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+echo "Removing duplicates in the 'Open With' menu"
+/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
+
+################################################################################
+# General Power and Performance modifications
+################################################################################
+
+echo "Disable the sudden motion sensor (it's not useful for SSDs/current MacBooks)"
+sudo pmset -a sms 0
+
+# Disable hibernation (speeds up entering sleep mode)
+sudo pmset -a hibernatemode 0
+
+echo "Speeding up wake from sleep to 24 hours from an hour"
+# http://www.cultofmac.com/221392/quick-hack-speeds-up-retina-macbooks-wake-from-sleep-os-x-tips/
+sudo pmset -a standbydelay 86400
+
+################################################################################
+# Trackpad, mouse, keyboard, Bluetooth accessories, and input
+################################################################################
+
+echo "Increasing sound quality for Bluetooth headphones/headsets"
+sudo defaults write bluetoothaudiod "Enable AptX codec" -bool true
+sudo defaults write bluetoothaudiod "Enable AAC codec" -bool false
+sudo defaults write bluetoothaudiod "AAC Bitrate" 320
+# defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Max (editable)" 80
+# defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" 80
+# defaults write com.apple.BluetoothAudioAgent "Apple Initial Bitpool (editable)" 80
+# defaults write com.apple.BluetoothAudioAgent "Apple Initial Bitpool Min (editable)" 80
+# defaults write com.apple.BluetoothAudioAgent "Negotiated Bitpool" 80
+# defaults write com.apple.BluetoothAudioAgent "Negotiated Bitpool Max" 80
+# defaults write com.apple.BluetoothAudioAgent "Negotiated Bitpool Min" 80
+
+# echo "Disable display from automatically adjusting brightness"
+# sudo defaults write /Library/Preferences/com.apple.iokit.AmbientLightSensor "Automatic Display Enabled" -bool false
+
+# echo "Disable keyboard from automatically adjusting backlight brightness in low light"
+# sudo defaults write /Library/Preferences/com.apple.iokit.AmbientLightSensor "Automatic Keyboard Enabled" -bool false
+
+################################################################################
+# Screen
+################################################################################
+
+echo "Requiring password immediately after sleep or screen saver begins"
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# echo "Enabling subpixel font rendering on non-Apple LCDs"
+# defaults write NSGlobalDomain AppleFontSmoothing -int 2
+
+################################################################################
+# Finder
+################################################################################
+
+echo "Show hidden files in Finder by default"
+defaults write com.apple.Finder AppleShowAllFiles -bool true
+
+echo "Show dotfiles in Finder by default"
+defaults write com.apple.finder AppleShowAllFiles TRUE
+
+echo "Show all filename extensions in Finder by default"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+echo "Show status bar in Finder by default"
+defaults write com.apple.finder ShowStatusBar -bool true
+
+echo "Disable the warning when changing a file extension"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+################################################################################
+# Time Machine
+################################################################################
+
+# echo "Prevent Time Machine from prompting to use new hard drives as backup volume"
+# defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+# echo "Disable local Time Machine snapshots"
+# sudo tmutil disablelocal
